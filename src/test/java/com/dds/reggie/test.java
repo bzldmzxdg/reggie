710b853ab7808c37eab7940f6c4173c85e47deef
dds <dds@qq.com>
2022-08-22 13:35:34 +0800

    一点小改动不用深究

--- src/test/java/com/dds/reggie/test.java
+++ src/test/java/com/dds/reggie/test.java
@@ -3,6 +3,7 @@ package com.dds.reggie;
 import com.dds.reggie.entity.R;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
 
 @SpringBootTest
 class test {
